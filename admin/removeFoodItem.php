b6fc09047d5f3d31990a199121276d13c56143da
subho <subho@DESKTOP-LCBPGN0>
2015-12-22 15:19:12 +0530

    Ready for Release.

--- admin/removeFoodItem.php
+++ admin/removeFoodItem.php
@@ -9,18 +9,17 @@ require("navbar.php");
 ?>
 <html>
     <head>
-        <title>PM - Delete Food Item </title>
-	
-        <script type="text/javascript">
-        $(function() {
-	
-            //autocomplete
-            $(".auto").autocomplete({
-		source: "search.php",
-		minLength: 1
-            });				
-        });
-        </script>
+        <title>PM - Delete Food Item </title
+        <link rel="stylesheet" type="text/css" href="auto/jquery.autocomplete.css" />
+<script type="text/javascript" src="auto/jquery.js"></script>
+<script type="text/javascript" src="auto/jquery.autocomplete.js"></script>
+        <script>
+ $(document).ready(function(){
+  $("#tag").autocomplete("autocomplete.php", {
+        selectFirst: true
+  });
+ });
+</script>
 		
     </head>
     <body class="landing">
@@ -31,12 +30,9 @@ require("navbar.php");
                     
             <form method="POST" action="DBHandler.php" class="col s12"  >
                 <div class="row">
-                    <div class="row">
-                        <div class="input-field col s6">
+                    <div class="input-field col s6">
                             <span>Food Name</span>
-                            <input placeholder="Search for Food Item" name="ser" type="text" class="auto" id="searchq">
-          
-                        </div>
+                            <input name="fname" type="text" id="tag">
                     </div>
                     <div class="input-field col s6">
                         <button type="submit" name="delFood" class="waves-effect waves-light btn-large">Delete</button>
